de4fe464882656b7bce769fe67bfda98712dc548
victory-nwawunze <victorynwawunze@gmail.com>
2022-01-22 04:41:46 -0800

    just added the click me function button

--- static/js/script.js
+++ static/js/script.js
@@ -4,5 +4,11 @@
 function ageInDays() {
   var birthYear = prompt("what year were you born........my good friend?");
   var ageInDayss = (2018 - birthYear) * 365;
-  console.log(ageInDayss);
+  var h1 = document.createElement("h1");
+  var textAnswer = document.createTextNode(
+    "You are " + ageInDayss + " days old"
+  );
+  h1.setAttribute("id", "ageInDays");
+  h1.appendChild(textAnswer);
+  document.getElementById("flex-box-result").appendChild(h1);
 }
