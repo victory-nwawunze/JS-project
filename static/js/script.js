b44dd1bba65a3cdb56e8069ad59c327479593a99
victory-nwawunze <victorynwawunze@gmail.com>
2022-01-29 08:32:26 -0800

    successfully updated the Hit and Stand functionality

--- static/js/script.js
+++ static/js/script.js
@@ -229,26 +229,32 @@ function showCard(card, activePlayer) {
 }
 
 function blackjackDeal() {
-  let yourImages = document.querySelector("#your-box").querySelectorAll("img");
-  let dealerImages = document
-    .querySelector("#dealer-box")
-    .querySelectorAll("img");
+  if (blackjackGame["turnsOver"] === true) {
+    blackjackGame["isStand"] = false;
+    let yourImages = document
+      .querySelector("#your-box")
+      .querySelectorAll("img");
+    let dealerImages = document
+      .querySelector("#dealer-box")
+      .querySelectorAll("img");
 
-  for (i = 0; i < yourImages.length; i++) {
-    yourImages[i].remove();
-  }
+    for (i = 0; i < yourImages.length; i++) {
+      yourImages[i].remove();
+    }
 
-  for (i = 0; i < dealerImages.length; i++) {
-    dealerImages[i].remove();
+    for (i = 0; i < dealerImages.length; i++) {
+      dealerImages[i].remove();
+    }
+    YOU["score"] = 0;
+    DEALER["score"] = 0;
+    document.querySelector("#your-blackjack-result").textContent = 0;
+    document.querySelector("#dealer-blackjack-result").textContent = 0;
+    document.querySelector("#your-blackjack-result").style.color = "ffffff";
+    document.querySelector("#dealer-blackjack-result").style.color = "ffffff";
+    document.querySelector("#blackjack-result").textContent = "Let's Play";
+    document.querySelector("#blackjack-result").style.color = "black";
+    blackjackGame["turnsOver"] = true;
   }
-  YOU["score"] = 0;
-  DEALER["score"] = 0;
-  document.querySelector("#your-blackjack-result").textContent = 0;
-  document.querySelector("#dealer-blackjack-result").textContent = 0;
-  document.querySelector("#your-blackjack-result").style.color = "ffffff";
-  document.querySelector("#dealer-blackjack-result").style.color = "ffffff";
-  document.querySelector("#blackjack-result").textContent = "Let's Play";
-  document.querySelector("#blackjack-result").style.color = "black";
 }
 
 function updateScore(card, activePlayer) {
@@ -311,22 +317,23 @@ function computeWinner() {
 
 function showResult(winner) {
   let message, messageColor;
-
-  if (winner === YOU) {
-    document.querySelector("#wins").textContent = blackjackGame["wins"];
-    message = "You won";
-    messageColor = "green";
-    winSound.play();
-  } else if (winner === DEALER) {
-    document.querySelector("#losses").textContent = blackjackGame["losses"];
-    message = "You lost";
-    messageColor = "red";
-    lossSound.play();
-  } else {
-    document.querySelector("#draws").textContent = blackjackGame["draws"];
-    message = "You drew!";
-    messageColor = "black";
+  if (blackjackGame["turnsOver"] === true) {
+    if (winner === YOU) {
+      document.querySelector("#wins").textContent = blackjackGame["wins"];
+      message = "You won";
+      messageColor = "green";
+      winSound.play();
+    } else if (winner === DEALER) {
+      document.querySelector("#losses").textContent = blackjackGame["losses"];
+      message = "You lost";
+      messageColor = "red";
+      lossSound.play();
+    } else {
+      document.querySelector("#draws").textContent = blackjackGame["draws"];
+      message = "You drew!";
+      messageColor = "black";
+    }
+    document.querySelector("#blackjack-result").textContent = message;
+    document.querySelector("#blackjack-result").style.color = messageColor;
   }
-  document.querySelector("#blackjack-result").textContent = message;
-  document.querySelector("#blackjack-result").style.color = messageColor;
 }
